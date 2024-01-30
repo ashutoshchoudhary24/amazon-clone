1ce138b47dbab14bfc41444d269be92c204c8e00
ashutoshchoudhary24 <ashumm.1807@gmail.com>
2024-01-30 21:44:55 +0530

    updated

--- src/Login.js
+++ src/Login.js
@@ -1,52 +1,57 @@
 import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import './Login.css'
-import { Link } from 'react-router-dom'
 import { auth } from './firebase'
-import {  useNavigate } from 'react-router-dom'
 
 function Login() {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-     const signIn = e => {
-        e.preventDefault();
-        //some firebase  login happens here    }
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const register = e => {
-        e.preventDefault();
-        // here do some firebase register
-        auth.createUserWithEmailAndPassword(email,password)
-            .then((auth) => {
-                navigate.push('/')
-                //it successfully craeated a new user with email and oasswrod
-                console.log(auth);
-            })
-            .catch(error => alert(error.message))
-    }
+  const login = event => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // Logged in and redirect to home page
+        navigate('/');
+      })
+      .catch((e) => alert(e.message));
+  }
+
+  const register = event => {
+    event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(auth => {
+        // Create a user, login and redirect to home page
+        navigate('/');
+      })
+      .catch((e) => alert(e.message));
+  }
+
   return (
     <div className='login'>
-        <Link to="/">
-             <img
-              className='login__logo' src="https://pngimg.com/uploads/amazon/amazon_PNG1.png" alt="amazon-logo" />
-        </Link>
-        {/* The login container is going to contain all of the usere inputs, email,paswrd */}
-        <div className="login__container">
-            <h1>Sign-in</h1>
+      <Link to="/">
+        <img className='login__logo' src="https://pngimg.com/uploads/amazon/amazon_PNG1.png" alt="" />
+      </Link>
+      <div className="login__container">
+        <h1>Sign In</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input value={email} onChange={event => setEmail(event.target.value)} type="email" />
+          <h5>Password</h5>
+          <input value={password} onChange={event => setPassword(event.target.value)} type="password" />
+          <button onClick={login} type='submit' className='login__signInButton'>Sign in</button>
+        </form>
+        <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
+        <button onClick={register} className='login__registerButton'>Create your Amazon account</button>
 
-            <form action="">
-                <h5>E-mail</h5>
-                <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
-                <h5>Password</h5>
-                <input type="password" value={password} onChange={ e => setPassword(e.target.value)} />
-            </form>
-            <button type='submit' onClick={signIn}
-            className='login__signInButton'>Login</button>
-            <p>By continuing, you agree to Amazon's Clone Conditions of Use and Privacy Notice.</p>
-            <button onClick='register' className='login__registerButton'>Create your Amazon  Account</button>
-        </div>
+      </div>
     </div>
   )
-}
 }
 
 export default Login
